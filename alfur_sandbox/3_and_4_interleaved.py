a3d369b80966079d0a10f78c1b32fa84ff4ef739
Alfurb <alfur.birkir@gmail.com>
2017-01-30 15:32:19 +0000

    First iteration of finding number of sets of perms with at least one perm of length 3 and one of length 4. Result: 14181

---
+++ alfur_sandbox/3_and_4_interleaved.py
@@ -0,0 +1,50 @@
+from permuta import *
+from permuta.misc.symmetry import *
+
+
+def all_sets_of_perms_of_set(perms):
+    all_sets = []
+    all_perms = list(perms)
+    for i in range(2**len(all_perms)):
+        temp_set = []
+        my_count = 0
+        while i > 0:
+            if i % 2 == 1:
+                temp_set.append(all_perms[my_count])
+            my_count += 1
+            i //= 2
+        all_sets.append(tuple(temp_set))
+    return sorted(set(all_sets))
+
+
+def perms_not_covered_by(perms_to_cover, coverers):
+    covered = set()
+    for coverer in coverers:
+        for perm in perms_to_cover:
+            if perm.contains(coverer):
+                covered.add(perm)
+    return sorted(set(perms_to_cover).difference(covered))
+
+
+def len_3_and_4_interleaved():
+    output_set = []
+    len_3 = all_sets_of_perms_of_set(list(PermSet(4)))
+    len_3 = sorted(set([lex_min(x) for x in len_3]))
+    len_3.remove(())
+    len_4 = list(PermSet(4))
+
+    for perms_3 in len_3:
+        temp_len_4 = perms_not_covered_by(len_4, perms_3)
+        for perms_4 in all_sets_of_perms_of_set(temp_len_4):
+            if perms_4 == ():
+                continue
+            output_set.append((*perms_3, *perms_4))
+    print(len(output_set))
+    return sorted(set([lex_min(x) for x in output_set]))
+
+
+
+if __name__ == "__main__":
+    print((all_sets_of_perms_of_set([Perm((1, 2, 0)), Perm((0, 1, 2)), Perm(())])))
+    print(perms_not_covered_by([Perm((1, 2, 0)), Perm((0, 1, 2))], [Perm((1, 0))]))
+    print(len(len_3_and_4_interleaved()))
