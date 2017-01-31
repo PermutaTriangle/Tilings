fdc0869c3e30c5e89582ab9c864cee347c5c9e4c
RagnarArdal <RagnarArdal@gmail.com>
2017-01-31 13:29:49 +0000

    I modified evvrything

--- grids/test.py
+++ grids/test.py
@@ -7,34 +7,34 @@ class TestTilingRank(unittest.TestCase):
         self.assertEqual(Tiling({}).rank(), 0)
 
     def test_points_no_clash(self):
-        self.assertEqual(Tiling({(0,0): Tile.P}).rank(), 1)
-        self.assertEqual(Tiling({(0,0): Tile.P, (1,1): Tile.P, (2,2): Tile.P}).rank(), 1)
+        self.assertEqual(Tiling({(0,0): Block.point}).rank(), 1)
+        self.assertEqual(Tiling({(0,0): Block.point, (1,1): Block.point, (2,2): Block.point}).rank(), 1)
 
     def test_sets_no_clash(self):
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (1,1): Tile.P, (3,4): Tile.INCREASING}).rank(), 2)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (1,1): Block.point, (3,4): Block.increasing}).rank(), 2)
 
     def test_point_point_clash(self):
-        self.assertEqual(Tiling({(0,0): Tile.P, (1,0): Tile.P}).rank(), 3)
+        self.assertEqual(Tiling({(0,0): Block.point, (1,0): Block.point}).rank(), 3)
 
     def test_set_point_clash(self):
-        self.assertEqual(Tiling({(0,0): Tile.P, (0,1): Tile.DECREASING}).rank(), 4)
+        self.assertEqual(Tiling({(0,0): Block.point, (0,1): Block.decreasing}).rank(), 4)
 
     def test_point_L_clash(self):
-        self.assertEqual(Tiling({(0,0): Tile.P, (0,1): Tile.P, (1,0): Tile.P}).rank(), 5)
+        self.assertEqual(Tiling({(0,0): Block.point, (0,1): Block.point, (1,0): Block.point}).rank(), 5)
 
     def test_set_set_clash(self):
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (1,0): Tile.INCREASING}).rank(), 6)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (1,0): Block.increasing}).rank(), 6)
 
     def test_other_clashes(self):
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (1,1): Tile.INCREASING, (1,2): Tile.P, (2,1): Tile.DECREASING}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (0,1): Tile.INCREASING, (1,0): Tile.INCREASING, (1,1): Tile.DECREASING}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (0,1): Tile.INCREASING, (1,0): Tile.INCREASING}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.P, (0,1): Tile.INCREASING, (1,0): Tile.INCREASING}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (0,1): Tile.P, (1,0): Tile.INCREASING}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (0,1): Tile.INCREASING, (1,0): Tile.P}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.P, (0,1): Tile.INCREASING, (1,0): Tile.P}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.DECREASING, (0,1): Tile.P, (1,0): Tile.P}).rank(), 7)
-        self.assertEqual(Tiling({(0,0): Tile.P, (0,1): Tile.P, (1,0): Tile.DECREASING}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (1,1): Block.increasing, (1,2): Block.point, (2,1): Block.decreasing}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (0,1): Block.increasing, (1,0): Block.increasing, (1,1): Block.decreasing}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (0,1): Block.increasing, (1,0): Block.increasing}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.point, (0,1): Block.increasing, (1,0): Block.increasing}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (0,1): Block.point, (1,0): Block.increasing}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (0,1): Block.increasing, (1,0): Block.point}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.point, (0,1): Block.increasing, (1,0): Block.point}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.decreasing, (0,1): Block.point, (1,0): Block.point}).rank(), 7)
+        self.assertEqual(Tiling({(0,0): Block.point, (0,1): Block.point, (1,0): Block.decreasing}).rank(), 7)
 
 
 
