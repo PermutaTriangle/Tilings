6e127ec5443319b821d24a7c279c33045195f5d7
Tagl <arnara14@ru.is>
2017-02-01 15:55:22 +0000

    python3.3 compatible

--- grids/parser.py
+++ grids/parser.py
@@ -22,7 +22,6 @@ def parse_log(inp, file=False):
     # +-+-+-+-+
     # 1: Av([[2, 1]])
     for i, el in enumerate(split(r'\d+:\s?\n[01]+', inp)[1:]):
-        print("blaasdfadsfafsdadfsafdsadfsasdfadsfadsfadsfasfasfafsdsadfafsd", el)
         # We always take the first rule in a block, they are seperated by two newlines
         item = el.strip().split('\n\n')[0]
         table = []
@@ -91,7 +90,7 @@ def tiling_to_json(tilings):
         tile = []
         for k, v in tiling.items():
             point = {}
-            point['point'] = [*k]
+            point['point'] = [k[0],k[1]]
             point['val'] = repr(v)
             tile.append(point)
         tiles.append(tile)
@@ -115,5 +114,5 @@ def tiling_to_json(tilings):
 if __name__ == '__main__':
     #print(tiling_to_json([Tiling({ (0,0): PermSet.avoiding(Perm.one([1,2,3])) }),
     #                      Tiling({ (0,3): Block.point, (1,0): Block.point, (2,1): PermSet.avoiding([Perm.one([1,2])]), (3,4): Block.point, (4,2): Block.point })]))
-    print(parse_log('../1234_1243_1324_1342_1423_1432_2134_2143_2314_2341_2413_2431_3124_3142_3214_3241_4123_4132_4213.txt', file=True))
+    print(parse_log('/data/henningu/length19/1234_1243_1324_1342_1423_1432_2134_2143_2314_2341_2413_2431_3124_3142_3214_3241_4123_4132_4213.txt', file=True))
     #print(parse_log('../../Parse/ex9_output.txt', file=True))
