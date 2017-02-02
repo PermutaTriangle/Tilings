ee741faba7705f6cc563a55b62beab485c1c5402
Sigurjón Freyr <siggifreyr1@gmail.com>
2017-02-02 15:37:30 +0000

    Initial Cover class definition, of_length not working it seems.

---
+++ grids/Cover.py
@@ -0,0 +1,52 @@
+# TODO: Docstring
+# TODO: Make python2.7 compatible once permuta is
+
+import warnings
+
+from builtins import list
+from operator import itemgetter
+from permuta import Perm
+from permuta import PermSet
+from permuta.misc import ordered_set_partitions, flatten
+from permuta.descriptors import Descriptor
+from Tiling import PermSetTiled
+
+class Cover(list):
+    """Cover class.
+
+    Tilings are stored in an array in the order they were initialized in.
+    """
+
+    input_set = PermSet()
+    def __init__(self, input_set, tilings=[]):
+        """Accepts a list of Tilings"""
+        list.__init__(self, tilings)
+        self.input_set = input_set
+
+
+    def __repr__(self):
+        format_string = "<A cover of {} tilings>"
+        return format_string.format(len(self))
+
+    def __str__(self):
+        pass
+
+    def of_length(self, num):
+        # Create examples
+        ret = []
+        for T in self:
+            print(T)
+            for perm in PermSetTiled(T).of_length(num):
+                ret.append(perm)
+        return ret
+
+if __name__ == "__main__":
+    from Cover import Cover
+    from Tiling import Tiling, Block
+    b = Tiling({(0,0): Block.point, (1,1): Block.point})
+    c = Tiling({(0,1): Block.point, (1,0): Block.point})
+    from permuta import PermSet, Perm
+    g = Cover(PermSet.avoiding([Perm.one([1,4,3,2])]), [c])
+
+    print(g.__repr__())
+    print(g.of_length(3))
