d6c7e3dd4e187439ede0745ef25d7bdf5b32fb9c
Tagl <arnara14@ru.is>
2017-02-10 13:30:43 +0000

    updated website to be pretty

--- web/server.py
+++ web/server.py
@@ -1,6 +1,8 @@
+from random import randint
 from flask import Flask
 from flask import abort, jsonify, redirect, url_for, render_template
 from flask_pymongo import PyMongo
+from jinja2 import Environment
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'permsdb'
 app.config['MONGO_HOST'] = 'localhost'
@@ -11,6 +13,13 @@ mongo = PyMongo(app)
 
 def render_our(*args, **kwargs):
     return render_template(args[0], **kwargs, int=int, enumerate=enumerate)
+
+def av_list_to_url(v):
+    return '_'.join([''.join(x.split(', ')) for x in v[5:-3].split('], [')])
+
+def av_list_to_str(v):
+    return "Av(" + ', '.join([''.join(x.split(', ')) for x in v[5:-3].split('], [')]) + ")"
+
 @app.route("/")
 def index():
     return redirect(url_for("permsroot"))
@@ -19,6 +28,12 @@ def index():
 def permsroot():
     return "<h1>Welcome to the Online Permutation Attribute Library</h1><br/><h3>Made by: Arnar, Alfur, Sigurjon and Unnar</h3>"
 
+@app.route("/perms/random/")
+def randav():
+    sz = mongo.db.perm.count()
+    n = randint(0,sz-1)
+    return redirect(url_for("avget", patterns=mongo.db.perm.find().limit(-1).skip(n).next()["avoid"]))
+
 @app.route("/perms/av/")
 def avroot():
     return redirect(url_for('permsroot'))
@@ -26,11 +41,6 @@ def avroot():
 @app.route("/perms/av/<patterns>/")
 def avget(patterns):
     val = mongo.db.perm.find_one_or_404({"avoid":patterns})
-    return jsonify({'avoid':val['avoid'],'tile':val['tile'],'examples':val['examples'],'recurrence':val['recurrence'],'genf':val['genf'],'solved_genf':val['solved_genf'],'coeffs':val['coeffs']})
-
-@app.route("/perms/avtest")
-def testing():
-    val = mongo.db.perm.find_one_or_404({"avoid":"o"})
     parsed_tilings = []
     maxi = []
     maxj = []
@@ -39,18 +49,17 @@ def testing():
         for tile in tiling:
             i, j = map(int, tile['point'])
             obj[(i, j)] = tile['val']
-        maxi.append(max( [ i for i, _ in obj ] ) + 1)
-        maxj.append(max( [ j for _, j in obj ] ) + 1)
+        maxi.append(max( [ i for i, _ in obj ] ) + 1 if obj else 1)
+        maxj.append(max( [ j for _, j in obj ] ) + 1 if obj else 1)
         parsed_tilings.append(obj)
     val['tile'] = parsed_tilings
 
-    # Test data
-    val['tile'].append({(2,3): '1', (0,1): '3', (1,2): '2', (3,0): '4'})
-    maxi.append(4)
-    maxj.append(4)
-    val['examples']['3'] = ['123', '321', '132']
-    val['examples']['2'] = ['12']
     return render_our('pattern.html', val=val, maxi=maxi, maxj=maxj)
+
+# set custom filters
+app.jinja_env.filters['av_list_to_url'] = av_list_to_url
+app.jinja_env.filters['av_list_to_str'] = av_list_to_str
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
