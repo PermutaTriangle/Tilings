69534c7c0114f1d170f5635b79965634394a0a2d
Tomas Ken <tomkenmag@gmail.com>
2017-10-11 12:25:00 +0000

    Adding tests written so far

--- tests/test_tiling.py
+++ tests/test_tiling.py
@@ -1,5 +1,7 @@
+import pytest
+
+from grids_two import Obstruction, Tiling
 from permuta import Perm
-from grids_two import Tiling, Obstruction
 
 
 def test_compression():
@@ -27,3 +29,40 @@ def test_compression():
             Obstruction(Perm((2, 1, 0)), ((2, 0), (2, 0), (3, 0)))])
 
     assert tiling == Tiling.decompress(tiling.compress())
+
+
+@pytest.fixture
+def smallrotatetil():
+    return Tiling(
+        point_cells=frozenset({(4, 0)}),
+        positive_cells=frozenset({(1, 1)}),
+        possibly_empty=frozenset({(1, 3), (3, 2)}),
+        obstructions=[Obstruction(Perm((0, 2, 1)),
+                                  ((1, 1), (1, 3), (3, 2)))])
+
+
+@pytest.fixture
+def rotatetil():
+    return Tiling(
+        point_cells=frozenset({(4, 0)}),
+        positive_cells=frozenset({(0, 0), (1, 2)}),
+        possibly_empty=frozenset({(2, 3), (3, 2)}),
+        obstructions=[Obstruction(Perm((0, 2, 1, 3)),
+                                  [(0, 0), (1, 2), (2, 1), (3, 2)])])
+
+
+def test_rotate(rotatetil, smallrotatetil):
+    assert smallrotatetil.rotate90() == Tiling(
+        point_cells=frozenset({(0, 0)}),
+        positive_cells=frozenset({(1, 2)}),
+        possibly_empty=frozenset({(2, 1), (3, 2)}),
+        obstructions=[Obstruction(Perm((2, 0, 1)),
+                                  ((1, 2), (2, 1), (3, 2)))])
+    print(rotatetil.rotate90())
+    assert rotatetil.rotate90() == Tiling(
+        point_cells=frozenset({(0, 0)}),
+        positive_cells=frozenset({(0, 4), (2, 3)}),
+        possibly_empty=frozenset({(1, 2), (3, 1)}),
+        obstructions=[Obstruction(Perm((3, 1, 2, 0)),
+                                  ((0, 4), (1, 2), (2, 3), (3, 1)))])
+    assert rotatetil.rotate180() == rotatetil.rotate90().rotate90()
