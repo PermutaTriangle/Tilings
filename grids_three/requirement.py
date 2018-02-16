48a5bb2d249d3f4a07a2651a8f88a5eaa518d172
Tomas Ken <tomkenmag@gmail.com>
2018-02-16 10:31:31 +0000

    Sorting imports of requirement

--- grids_three/requirement.py
+++ grids_three/requirement.py
@@ -1,7 +1,8 @@
+from permuta import Perm
+from permuta.misc import DIR_EAST, DIR_NONE, DIR_NORTH, DIR_SOUTH, DIR_WEST
+
 from .griddedperm import GriddedPerm
 from .obstruction import Obstruction
-from permuta.misc import DIR_EAST, DIR_NONE, DIR_WEST, DIR_NORTH, DIR_SOUTH
-from permuta import Perm
 
 
 class Requirement(GriddedPerm):
