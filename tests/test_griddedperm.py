4bc2d3109e17becb03de7417c75b55d7ebd0cbf7
Christian Bean <christianbean@ru.is>
2018-10-02 11:35:13 +0000

    some more tests

--- tests/test_griddedperm.py
+++ tests/test_griddedperm.py
@@ -36,6 +36,22 @@ def isolatedob():
                        ((0, 0), (1, 1), (2, 2)))
 
 
+def test_single_cell():
+    gp = GriddedPerm.single_cell(Perm((0, 1, 2)), (2, 3))
+    assert gp.patt == Perm((0, 1, 2))
+    assert gp.pos == ((2, 3), (2, 3), (2, 3))
+
+    gp = GriddedPerm.single_cell(Perm((0,)), (45, 64))
+    assert gp.patt == Perm((0,))
+    assert gp.pos == ((45, 64),)
+
+
+def test_empty_perm():
+    gp = GriddedPerm.empty_perm()
+    assert gp.patt == Perm(())
+    assert gp.pos == ()
+
+
 def test_contradictory(simpleob, singlecellob, everycellob):
     assert not simpleob.contradictory()
     assert not singlecellob.contradictory()
@@ -46,7 +62,7 @@ def test_contradictory(simpleob, singlecellob, everycellob):
     assert gp.contradictory()
 
 
-def test_occupies_cell(simpleob):
+def test_occupies(simpleob):
     assert simpleob.occupies((0, 0))
     assert simpleob.occupies((2, 1))
     assert simpleob.occupies((2, 2))
@@ -59,17 +75,22 @@ def test_occupies_cell(simpleob):
 def test_occurrences_in(simpleob):
     ob = GriddedPerm(Perm((0, 2, 1)), ((0, 0), (2, 2), (2, 1)))
     assert list(ob.occurrences_in(simpleob)) == [(0, 2, 3), (1, 2, 3)]
+    assert ob.occurs_in(simpleob)
 
     ob = GriddedPerm(Perm((1, 0, 2)), ((0, 0), (0, 0), (2, 1)))
     assert list(ob.occurrences_in(simpleob)) == [(0, 1, 3)]
+    assert ob.occurs_in(simpleob)
 
     ob = GriddedPerm(Perm((1, 0, 2)), ((0, 0), (0, 0), (2, 2)))
     assert list(ob.occurrences_in(simpleob)) == [(0, 1, 2)]
+    assert ob.occurs_in(simpleob)
 
     ob = GriddedPerm(Perm((0, 1, 2)), ((0, 0), (2, 2), (2, 1)))
     assert not any(ob.occurrences_in(simpleob))
+    assert not ob.occurs_in(simpleob)
 
     ob = GriddedPerm(Perm((1, 0, 2)), ((0, 0), (2, 2), (2, 2)))
+    assert not any(ob.occurrences_in(simpleob))
     assert not ob.occurs_in(simpleob)
 
 
@@ -91,6 +112,21 @@ def test_isolated_cells(simpleob, isolatedob):
     assert list(isolatedob.isolated_cells()) == [(0, 0), (1, 1), (2, 2)]
 
 
+def test_is_isolated(simpleob, isolatedob):
+    assert simpleob.is_isolated([2, 3])
+    assert simpleob.is_isolated([0, 1])
+    assert not simpleob.is_isolated([0])
+    assert not simpleob.is_isolated([2])
+    assert not simpleob.is_isolated([1, 3])
+    assert isolatedob.is_isolated([0, 1, 2])
+    assert isolatedob.is_isolated([0, 1])
+    assert isolatedob.is_isolated([0, 2])
+    assert isolatedob.is_isolated([1, 2])
+    assert isolatedob.is_isolated([0])
+    assert isolatedob.is_isolated([1])
+    assert isolatedob.is_isolated([2])
+
+
 def test_forced_point_index(singlecellob):
     assert singlecellob.forced_point_index((1, 1), DIR_SOUTH) is None
     assert singlecellob.forced_point_index((2, 2), DIR_WEST) == 0
@@ -99,7 +135,25 @@ def test_forced_point_index(singlecellob):
     assert singlecellob.forced_point_index((2, 2), DIR_EAST) == 3
 
 
-def test_get_rowcol(everycellob):
+def test_get_rowcol(everycellob, simpleob, typicalob):
+    assert list(simpleob.get_points_col(0)) == [(0, 1), (1, 0)]
+    assert list(simpleob.get_points_col(1)) == []
+    assert list(simpleob.get_points_col(2)) == [(2, 3), (3, 2)]
+    
+
+    assert list(typicalob.get_points_col(0)) == [(0, 1), (1, 0)]
+    assert list(typicalob.get_points_col(1)) == [(2, 2), (3, 4), (4, 3)]
+    assert list(typicalob.get_points_col(2)) == []
+
+    assert list(simpleob.get_points_row(0)) == [(0, 1), (1, 0)]
+    assert list(simpleob.get_points_row(1)) == [(3, 2)]
+    assert list(simpleob.get_points_row(2)) == [(2, 3)]
+    assert list(simpleob.get_points_row(3)) == []
+
+    assert list(typicalob.get_points_row(0)) == [(0, 1), (1, 0), (2, 2)]
+    assert list(typicalob.get_points_row(1)) == [(3, 4), (4, 3)]
+    assert list(typicalob.get_points_row(2)) == []
+
     assert list(everycellob.get_points_row(1)) == [(1, 3), (4, 4), (7, 5)]
     assert list(everycellob.get_points_col(1)) == [(3, 1), (4, 4), (5, 7)]
     assert list(everycellob.get_points_above_row(2)) == []
@@ -171,18 +225,34 @@ def test_place_point(typicalob):
                      ((2, 0), (2, 0), (3, 0), (3, 3), (3, 1))),
          GriddedPerm(Perm((1, 0, 2, 4, 3)),
                      ((2, 0), (2, 0), (3, 0), (3, 3), (3, 3)))])
-    assert (list(GriddedPerm(Perm((2, 1, 0, 4, 3)),
+    print((list(sorted(GriddedPerm(Perm((2, 1, 0, 4, 3)),
                              ((0, 1), (0, 1), (1, 0), (2, 1), (2, 1))
                              ).place_point(
-                                 (2, 1), DIR_SOUTH)) ==
+                                 (2, 1), DIR_SOUTH)))))
+    assert (list(sorted(GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                             ((0, 1), (0, 1), (1, 0), (2, 1), (2, 1))
+                             ).place_point(
+                                 (2, 1), DIR_SOUTH))) ==
             [GriddedPerm(Perm((2, 1, 0, 3)),
                          ((0, 1), (0, 1), (1, 0), (2, 3))),
              GriddedPerm(Perm((2, 1, 0, 4, 3)),
-                         ((0, 3), (0, 3), (1, 0), (4, 3), (4, 3))),
+                         ((0, 1), (0, 1), (1, 0), (2, 3), (2, 3))),
+             GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                         ((0, 1), (0, 1), (1, 0), (2, 3), (4, 3))),
+             GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                         ((0, 1), (0, 1), (1, 0), (4, 3), (4, 3))),
+             GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                         ((0, 3), (0, 1), (1, 0), (2, 3), (2, 3))),
+             GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                         ((0, 3), (0, 1), (1, 0), (2, 3), (4, 3))),
              GriddedPerm(Perm((2, 1, 0, 4, 3)),
                          ((0, 3), (0, 1), (1, 0), (4, 3), (4, 3))),
              GriddedPerm(Perm((2, 1, 0, 4, 3)),
-                         ((0, 1), (0, 1), (1, 0), (4, 3), (4, 3)))])
+                         ((0, 3), (0, 3), (1, 0), (2, 3), (2, 3))),
+             GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                         ((0, 3), (0, 3), (1, 0), (2, 3), (4, 3))),
+             GriddedPerm(Perm((2, 1, 0, 4, 3)),
+                         ((0, 3), (0, 3), (1, 0), (4, 3), (4, 3)))])
 
     def minimize(obba):
         obba = sorted(obba)
