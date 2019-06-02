a531fb929de74bc8fdde9a76291e484f9f427bd7
Christian Bean <christianbean@ru.is>
2019-06-02 22:18:50 +0000

    Update setup file

--- setup.py
+++ setup.py
@@ -9,13 +9,19 @@ def read(fname):
 
 
 setup(
-    name="grids_three",
-    version="0.1.0",
-    author="Henning Ulfarsson",
-    author_email="henningu@ru.is",
-    maintainer="Christian Nathaniel Bean",
-    maintainer_email="christianbean@ru.is",
+    name="tilings",
+    version="0.0.1",
+    author="Permuta Triangle",
+    author_email="permutatriangle@gmail.com",
+    description="A Python library for gridded permutations and tilings.",
+    license="BSD-3",
+    keywords=("permutation perm gridded pattern tiling avoid contain"
+              "occurrences grid class")
     url="https://github.com/PermutaTriangle/grids_three",
+    project_urls={
+        'Source': 'https://github.com/PermutaTriangle/grids_three',
+        'Tracker': 'https://github.com/PermutaTriangle/grids_three/issues'
+    },
     packages=find_packages(),
     long_description=read("README.md"),
     install_requires=['comb_spec_searcher',
@@ -26,4 +32,20 @@ setup(
                    'permuta',
                    'pytest-pep8',
                    'pytest-isort']
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
 )
