446d39e797b75ed23083a3eb8239212e69c0a55f
Émile Nadeau <nadeau.emile@gmail.com>
2019-09-17 17:08:59 +0000

    Component fusion algorithm

--- tilings/algorithms/__init__.py
+++ tilings/algorithms/__init__.py
@@ -1,5 +1,5 @@
 from .factor import (Factor, FactorWithInterleaving,
                      FactorWithMonotoneInterleaving)
-from .fusion import Fusion
+from .fusion import ComponentFusion, Fusion
 from .obstruction_transitivity import ObstructionTransitivity
 from .row_col_separation import RowColSeparation
