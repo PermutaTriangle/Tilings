609c24346dcb8ca2acfe933737cd041e8a09d946
requires.io <support@requires.io>
2020-01-17 11:35:19 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
                       'pymongo==3.10.1',
                       'sympy==1.5.1'],
     setup_requires=['pytest-runner==5.2'],
-    tests_require=['pytest==5.3.2',
+    tests_require=['pytest==5.3.3',
                    'pytest-cov==2.8.1',
                    'pytest-pep8==1.0.6',
                    'pytest-isort==0.3.1'],
