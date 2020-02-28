44c6bfc33af2f395a593d56fbb6ceae65a7d4f66
Émile Nadeau <nadeau.emile@gmail.com>
2020-02-28 12:27:58 +0000

    adding cli code

--- setup.py
+++ setup.py
@@ -46,4 +46,10 @@ setup(
         'Topic :: Education',
         'Topic :: Scientific/Engineering :: Mathematics',
     ],
+    entry_points={
+        'console_scripts': [
+            'tilescope=tilings.cli:main',
+        ]
+    }
+
 )
