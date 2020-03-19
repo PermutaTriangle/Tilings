cd34f0099a840a3b605a6dbe209fec64ae21b7c2
Christian Bean <christianbean@ru.is>
2020-03-19 16:13:39 +0100

    Use Counter class, remove overly verbose use of heapq

--- tilings/algorithms/minimal_gridded_perms.py
+++ tilings/algorithms/minimal_gridded_perms.py
@@ -1,17 +1,19 @@
-from collections import defaultdict
+from collections import Counter
 from heapq import heapify, heappop, heappush
 from itertools import chain, product
+from typing import Iterator, List, Optional, Tuple, Union
 
-from typing import DefaultDict, Iterator, List, Optional, Tuple
 from tilings import GriddedPerm
 from tilings.misc import union_reduce
 
+
 Cell = Tuple[int, int]
-Counter = DefaultDict[Cell, int]
+
 
 class Info(tuple):
     def __lt__(self, other):
         return self[0].__lt__(other[0])
+
 
 class MinimalGriddedPerms(object):
     def __init__(self, tiling):
@@ -23,19 +25,6 @@ class MinimalGriddedPerms(object):
         self.yielded = set()
         self.queue = []
         heapify(self.queue)
-
-    def pop(self) -> Info:
-        """Return next Info on the queue."""
-        info = heappop(self.queue)
-        return info
-
-    def put(self, info: Info) -> None:
-        """Put into onto the queue."""
-        heappush(self.queue, info)
-
-    def empty_queue(self) -> bool:
-        """Return true if the queue is empty."""
-        return not bool(self.queue)
 
     def satisfies_obstructions(self, gp: GriddedPerm) -> bool:
         """Check if a gridded permutation avoids all obstructions."""
@@ -59,30 +48,21 @@ class MinimalGriddedPerms(object):
             max_cell_counts = []
             cells = set()
             for gps in product(*self.requirements[1:]):
-                max_cell_count = defaultdict(int)
-                for othergp in gps:
-                    max_cell_count = self.cell_counter(othergp, max_cell_count)
-                max_cell_count = self.cell_counter(gp, max_cell_count)
+                max_cell_count = self.cell_counter(*gps, gp)
                 max_cell_counts.append(max_cell_count)
                 cells.update(max_cell_count)
-
             new_info = Info([gp, max_cell_counts, cells])
-            self.put(new_info)
+            heappush(self.queue, new_info)
 
     @staticmethod
-    def cell_counter(
-                     gp: GriddedPerm, counter: Optional[Counter] = None
-                     ) -> Counter:
-        """Returns a counter of the number of cells used by the gridded
-        permutation. If 'counter' is given, will update this Counter."""
-        if counter is None:
-            counter = defaultdict(int)
-        for cell in gp.pos:
-            counter[cell] += 1
-        return counter
+    def cell_counter(*gps: GriddedPerm) -> Counter:
+        """Returns a counter of the sum of number of cells used by the gridded
+        permutations. If 'counter' is given, will update this Counter."""
+        return Counter(chain.from_iterable(gp.pos for gp in gps))
 
     def get_cells_to_try(
-                         self, gp: GriddedPerm, max_cell_counts: List[Counter],
+                         self, gp: GriddedPerm,
+                         max_cell_counts: List[Counter],
                          cells: List[Cell]) -> Iterator[Cell]:
         """Yield cells that a gridded permutation could be extended by."""
         currcellcounts = self.cell_counter(gp)
@@ -102,10 +82,9 @@ class MinimalGriddedPerms(object):
         if len(self.requirements) == 1:
             yield from iter(self.requirements[0])
             return
-
         self.prepare_queue()
-        while not self.empty_queue():
-            startgp, max_cell_counts, cells = self.pop()
+        while self.queue:
+            startgp, max_cell_counts, cells = heappop(self.queue)
             for cell in self.get_cells_to_try(startgp, max_cell_counts, cells):
                 for gp in startgp.insert_point(cell):
                     if gp not in self.seen:
@@ -116,7 +95,8 @@ class MinimalGriddedPerms(object):
                                 self.yielded.add(gp)
                                 yield gp
                             else:
-                                self.put(Info([gp, max_cell_counts, cells]))
+                                heappush(self.queue,
+                                         Info([gp, max_cell_counts, cells]))
 
 
 
