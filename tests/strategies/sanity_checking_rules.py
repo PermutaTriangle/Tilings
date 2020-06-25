f0215f4d0aa1f832f0e0739081acf003c38c21a5
Christian Bean <christianbean@ru.is>
2020-06-25 21:24:11 +0200

    add another rule to sanity check in the future

--- tests/strategies/sanity_checking_rules.py
+++ tests/strategies/sanity_checking_rules.py
@@ -28,7 +28,39 @@ def multi_variable():
                     TrackingAssumption((GriddedPerm(Perm((0,)), ((0, 0),)),)),
                 ),
             )
-        )
+        ),
+        RequirementInsertionStrategy(
+            gps=frozenset({GriddedPerm(Perm((0,)), ((2, 0),))}), ignore_parent=True
+        )(
+            Tiling(
+                obstructions=(
+                    GriddedPerm(Perm((0, 1)), ((1, 0), (1, 0))),
+                    GriddedPerm(Perm((0, 1)), ((2, 0), (2, 0))),
+                    GriddedPerm(Perm((0, 1)), ((2, 0), (3, 0))),
+                    GriddedPerm(Perm((0, 1)), ((3, 0), (3, 0))),
+                    GriddedPerm(Perm((1, 0)), ((0, 0), (2, 0))),
+                    GriddedPerm(Perm((1, 0)), ((0, 0), (3, 0))),
+                    GriddedPerm(Perm((1, 0)), ((2, 0), (2, 0))),
+                    GriddedPerm(Perm((1, 0)), ((2, 0), (3, 0))),
+                    GriddedPerm(Perm((1, 0)), ((3, 0), (3, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 0), (0, 0), (0, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 0), (0, 0), (1, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 0), (0, 0), (2, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 0), (0, 0), (3, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 0), (1, 0), (2, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 0), (1, 0), (3, 0))),
+                    GriddedPerm(Perm((0, 2, 1)), ((0, 0), (0, 0), (1, 0))),
+                    GriddedPerm(Perm((1, 0, 2)), ((0, 0), (0, 0), (0, 0))),
+                ),
+                requirements=(
+                    (GriddedPerm(Perm((0,)), ((0, 0),)),),
+                    (GriddedPerm(Perm((0,)), ((3, 0),)),),
+                ),
+                assumptions=(
+                    TrackingAssumption((GriddedPerm(Perm((0,)), ((3, 0),)),)),
+                ),
+            )
+        ).to_reverse_rule(),
     ]
 
 
