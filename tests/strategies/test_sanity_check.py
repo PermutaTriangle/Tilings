053b43aa7bb9e190d2647068626bc23ec7377d08
Christian Bean <christianbean@ru.is>
2020-06-26 20:25:07 +0200

    add a fusion rule that gave an error previously

--- tests/strategies/test_sanity_check.py
+++ tests/strategies/test_sanity_check.py
@@ -3,6 +3,7 @@ import pytest
 from permuta import Perm
 from tilings import GriddedPerm, Tiling
 from tilings.assumptions import TrackingAssumption
+from tilings.strategies.fusion import FusionStrategy
 from tilings.strategies.requirement_insertion import RequirementInsertionStrategy
 
 
@@ -60,6 +61,65 @@ def rules_to_check():
         )
         .to_equivalence_rule()
         .to_reverse_rule(),
+        FusionStrategy(col_idx=1, tracked=True)(
+            Tiling(
+                obstructions=(
+                    GriddedPerm(Perm((0,)), ((0, 0),)),
+                    GriddedPerm(Perm((0,)), ((1, 0),)),
+                    GriddedPerm(Perm((0,)), ((1, 1),)),
+                    GriddedPerm(Perm((0,)), ((2, 0),)),
+                    GriddedPerm(Perm((0,)), ((2, 1),)),
+                    GriddedPerm(Perm((0,)), ((3, 1),)),
+                    GriddedPerm(Perm((0,)), ((3, 2),)),
+                    GriddedPerm(Perm((0, 1)), ((1, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 1)), ((1, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 1)), ((2, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 1)), ((3, 0), (3, 0))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 1), (0, 1), (0, 2))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 1), (0, 2), (0, 2))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 1), (0, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 1, 2)), ((0, 1), (0, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 2, 1)), ((0, 1), (0, 1), (0, 1))),
+                    GriddedPerm(Perm((0, 2, 1)), ((0, 1), (1, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 2, 1)), ((0, 1), (1, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 2, 1)), ((0, 1), (2, 2), (2, 2))),
+                    GriddedPerm(Perm((1, 0, 2)), ((0, 2), (0, 1), (1, 2))),
+                    GriddedPerm(Perm((1, 0, 2)), ((0, 2), (0, 1), (2, 2))),
+                    GriddedPerm(Perm((2, 0, 1)), ((0, 1), (0, 1), (0, 1))),
+                    GriddedPerm(Perm((0, 1, 3, 2)), ((0, 2), (0, 2), (0, 2), (0, 2))),
+                    GriddedPerm(Perm((0, 1, 3, 2)), ((0, 2), (0, 2), (0, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 1, 3, 2)), ((0, 2), (0, 2), (0, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 1, 3, 2)), ((0, 2), (0, 2), (1, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 1, 3, 2)), ((0, 2), (0, 2), (1, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 1, 3, 2)), ((0, 2), (0, 2), (2, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 2, 1, 3)), ((0, 2), (0, 2), (0, 2), (0, 2))),
+                    GriddedPerm(Perm((0, 2, 1, 3)), ((0, 2), (0, 2), (0, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 2, 1, 3)), ((0, 2), (0, 2), (0, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 2, 3, 1)), ((0, 2), (0, 2), (0, 2), (0, 2))),
+                    GriddedPerm(Perm((0, 2, 3, 1)), ((0, 2), (0, 2), (0, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 2, 3, 1)), ((0, 2), (0, 2), (0, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 2, 3, 1)), ((0, 2), (0, 2), (1, 2), (1, 2))),
+                    GriddedPerm(Perm((0, 2, 3, 1)), ((0, 2), (0, 2), (1, 2), (2, 2))),
+                    GriddedPerm(Perm((0, 2, 3, 1)), ((0, 2), (0, 2), (2, 2), (2, 2))),
+                    GriddedPerm(Perm((2, 0, 1, 3)), ((0, 2), (0, 2), (0, 2), (0, 2))),
+                    GriddedPerm(Perm((2, 0, 1, 3)), ((0, 2), (0, 2), (0, 2), (1, 2))),
+                    GriddedPerm(Perm((2, 0, 1, 3)), ((0, 2), (0, 2), (0, 2), (2, 2))),
+                ),
+                requirements=(
+                    (GriddedPerm(Perm((0,)), ((1, 2),)),),
+                    (GriddedPerm(Perm((0,)), ((2, 2),)),),
+                    (GriddedPerm(Perm((0,)), ((3, 0),)),),
+                ),
+                assumptions=(
+                    TrackingAssumption(
+                        (
+                            GriddedPerm(Perm((0,)), ((2, 2),)),
+                            GriddedPerm(Perm((0,)), ((3, 0),)),
+                        )
+                    ),
+                ),
+            )
+        ),
     ]
 
 
