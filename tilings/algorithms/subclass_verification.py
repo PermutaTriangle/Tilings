ecb7e53813d45d9527a19ccb081224c3146ee7b8
Jay Pantone <jay.pantone@gmail.com>
2020-07-22 22:56:30 -0500

    WIP - 'quick_pare' SCV shortcuts

--- tilings/algorithms/subclass_verification.py
+++ tilings/algorithms/subclass_verification.py
@@ -12,7 +12,8 @@ NOTES:
       depending on which requirement in a list you're placing.
 """
 
-from typing import TYPE_CHECKING, FrozenSet, List
+from time import time
+from typing import TYPE_CHECKING, List, Set
 
 from permuta import Perm
 from tilings.algorithms import GriddedPermsOnTiling
@@ -22,17 +23,67 @@ if TYPE_CHECKING:
 
 
 class SubclassVerificationAlgorithm:
-    def __init__(self, tiling: "Tiling", perms_to_check: FrozenSet[Perm]) -> None:
+    def __init__(self, tiling: "Tiling", perms_to_check: Set[Perm]) -> None:
         self.tiling = tiling
         self.perms_to_check = perms_to_check
 
+    def quick_pare(self) -> Set[Perm]:
+
+        if len(self.tiling.requirements) == 0:
+            perms_leftover = set()
+
+            cell_basis = self.tiling.cell_basis()
+            all_bases = set(tuple(obs) for obs, _ in cell_basis.values())
+            max_base_len = {base: max(len(b) for b in base) for base in all_bases}
+            for perm in self.perms_to_check:
+                if not any(
+                    len(perm) < max_base_len[base] or perm.avoids(*base)
+                    for base in all_bases
+                ):
+                    perms_leftover.add(perm)
+            return perms_leftover
+
+        if (
+            len(self.tiling.requirements) == 1
+            and len(self.tiling.requirements[0]) == 1
+            and len(self.tiling.requirements[0][0]) == 1
+        ):
+            pos_cell = self.tiling.requirements[0][0].pos[0]
+            pos_basis = self.tiling.cell_basis()[pos_cell][0]
+            max_pos_basis_len = max(len(b) for b in pos_basis)
+            perms_leftover = set()
+            for perm in self.perms_to_check:
+                if not (len(perm) < max_pos_basis_len or perm.avoids(*pos_basis)):
+                    perms_leftover.add(perm)
+            return perms_leftover
+
+        return self.perms_to_check
+
     def verified(self) -> bool:
-        max_length = self.tiling.maximum_length_of_minimum_gridded_perm() + max(
-            map(len, self.perms_to_check)
+        if len(self.tiling.point_cells) > 0:
+            return False
+
+        tt = time()
+
+        # print("=" * 100)
+        # print(self.tiling)
+        # traceback.print_stack()
+        perms_to_check = self.quick_pare()
+        pare_time = time() - tt
+        # print(perms_to_check)
+        if len(perms_to_check) == 0:
+            return False
+
+        max_len_of_perms_to_check = max(map(len, perms_to_check))
+        max_length = (
+            self.tiling.maximum_length_of_minimum_gridded_perm()
+            + max_len_of_perms_to_check
         )
 
-        GP = GriddedPermsOnTiling(self.tiling, max_length)
-        perms_left = set(self.perms_to_check)
+        GP = GriddedPermsOnTiling(self.tiling).gridded_perms(
+            max_length, place_at_most=max_len_of_perms_to_check
+        )
+        perms_left = set(perms_to_check)
 
         for gp in GP:
             to_remove: List[Perm] = []
@@ -42,5 +93,17 @@ class SubclassVerificationAlgorithm:
             for perm_to_remove in to_remove:
                 perms_left.remove(perm_to_remove)
             if len(perms_left) == 0:
+                total_time = time() - tt
+                print("=" * 100)
+                print(self.tiling)
+                print(pare_time)
+                print(total_time)
+                print(False)
                 return False
+        total_time = time() - tt
+        print("=" * 100)
+        print(self.tiling)
+        print(pare_time)
+        print(total_time)
+        print(True)
         return True
