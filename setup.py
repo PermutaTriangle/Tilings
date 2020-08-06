f326924e7c9aaede7ea502fa0b4643ff307e4055
requires.io <support@requires.io>
2020-08-06 14:31:40 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(),
     long_description=read("README.rst"),
     install_requires=[
-        "comb-spec-searcher==2.0.0",
+        "comb-spec-searcher==2.1.0",
         "permuta==2.0.1",
         "requests==2.24.0",
         "typing-extensions==3.7.4.2",
