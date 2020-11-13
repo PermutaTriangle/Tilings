2c2e2094f745d97056dfd938b6e8b159efc726a8
requires.io <support@requires.io>
2020-11-13 11:03:13 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "comb-spec-searcher==2.4.0",
         "permuta==2.0.2",
-        "requests==2.24.0",
+        "requests==2.25.0",
     ],
     python_requires=">=3.6",
     classifiers=[
