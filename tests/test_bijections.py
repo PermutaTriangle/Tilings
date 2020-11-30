0d9c8213da462459dc6e2d0bd5fe6ec99ef2a987
JonSteinn <jonsteinn@gmail.com>
2020-11-30 22:29:46 +0000

    tests for bijection

---
+++ tests/test_bijections.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+
+from comb_spec_searcher import CombinatorialSpecification
+
+data = Path(__file__).parent.joinpath('resources', 'specs')
+
+def test_bijections():
+    for char in "abc":
+        spec = CombinatorialSpecification.from_dict(
+            json.loads(data.joinpath(f"{char}1.json").read_text())
+        )
+        other = CombinatorialSpecification.from_dict(
+            json.loads(data.joinpath(f"{char}2.json").read_text())
+        )
+        bijection = spec.get_bijection_to(other)
+        assert all(
+            {bijection.map(gp) for gp in spec.generate_objects_of_size(i)}
+            == set(other.generate_objects_of_size(i))
+            for i in range(10)
+        )
