69eb2c7e68f086b23691bf59ad927c5259611250
Christian Bean <christianbean@ru.is>
2021-01-05 13:59:42 +0000

    add is_two_way method to strategies (#286)
    * add is_two_way method to strategy
    
    * sanity check for reverse disjoint rules
    
    * added test for cartesian product (and reverse) rules to sanity check tests
    
    * reverse constructor method to strats, not implemented
    
    * parent terms passed as arg to get_terms method of constructor
    
    * more factor sanity check
    
    * add rearrange assumptions strategy
    
    * fix rearrange strategy repr
    
    * use dummy constructor for reverse constructor
    
    * outline for rearrange constructor
    
    * counting for rearrange
    
    * removing duplicate can_be_equivalent method from RearrangeAssumptionStrategy
    
    * counting for rearrange
    
    * get object and sample from rearrange
    
    * clean and make the equation
    
    * reverse rearrange constructor weird implementation
    
    * fix get equation for reverse
    
    * splitting strategy reverse constructor
    
    * fix rearrange
    
    * fix tests
    
    * fusion is not equivalence
    
    * transition to new json
    
    * update css dependency
    
    * refactor fusion strategy
    
    * fix more pylint
    
    * move dummy constructor to it's own file
    
    * update CHANGELOG
    
    Co-authored-by: Émile Nadeau <nadeau.emile@gmail.com>

--- tests/strategies/test_sanity_check.py
+++ tests/strategies/test_sanity_check.py
@@ -1,7 +1,9 @@
 import pytest
 
+from comb_spec_searcher.strategies.constructor import CartesianProduct, DisjointUnion
 from tilings import GriddedPerm, Tiling
 from tilings.assumptions import TrackingAssumption
+from tilings.strategies.factor import FactorStrategy
 from tilings.strategies.fusion import FusionStrategy
 from tilings.strategies.requirement_insertion import RequirementInsertionStrategy
 from tilings.strategies.requirement_placement import RequirementPlacementStrategy
@@ -55,9 +57,7 @@ def rules_to_check():
                 ),
                 assumptions=(TrackingAssumption((GriddedPerm((0,), ((3, 0),)),)),),
             )
-        )
-        .to_equivalence_rule()
-        .to_reverse_rule(),
+        ).to_equivalence_rule(),
         FusionStrategy(col_idx=1, tracked=True)(
             Tiling(
                 obstructions=(
@@ -139,9 +139,76 @@ def rules_to_check():
                 requirements=((GriddedPerm((0,), ((0, 1),)),),),
                 assumptions=(TrackingAssumption((GriddedPerm((0,), ((0, 2),)),)),),
             )
-        )
-        .to_equivalence_rule()
-        .to_reverse_rule(),
+        ).to_equivalence_rule(),
+        FactorStrategy([[(0, 0)], [(1, 1)]])(
+            Tiling(
+                obstructions=(
+                    GriddedPerm((0, 1), ((0, 0), (0, 0))),
+                    GriddedPerm((1, 0), ((1, 1), (1, 1))),
+                ),
+            )
+        ),
+        FactorStrategy([[(0, 0)], [(1, 1)], [(2, 2)]])(
+            Tiling(
+                obstructions=(
+                    GriddedPerm((0, 1), ((0, 0), (0, 0))),
+                    GriddedPerm((1, 0), ((1, 1), (1, 1))),
+                    GriddedPerm((0, 1), ((2, 2), (2, 2))),
+                    GriddedPerm((1, 0), ((2, 2), (2, 2))),
+                ),
+                requirements=(
+                    (GriddedPerm((0,), ((0, 0),)),),
+                    (GriddedPerm((0,), ((2, 2),)),),
+                ),
+            )
+        ),
+        FactorStrategy([[(0, 0)], [(1, 1)]])(
+            Tiling(
+                obstructions=(
+                    GriddedPerm((0, 1), ((0, 0), (0, 0))),
+                    GriddedPerm((1, 0), ((1, 1), (1, 1))),
+                ),
+                assumptions=(TrackingAssumption((GriddedPerm((0,), ((0, 0),)),)),),
+            )
+        ),
+        FactorStrategy([[(0, 0)], [(1, 1)], [(2, 2)]])(
+            Tiling(
+                obstructions=(
+                    GriddedPerm((0, 1), ((0, 0), (0, 0))),
+                    GriddedPerm((1, 0), ((1, 1), (1, 1))),
+                    GriddedPerm((0, 1), ((2, 2), (2, 2))),
+                    GriddedPerm((1, 0), ((2, 2), (2, 2))),
+                ),
+                requirements=(
+                    (GriddedPerm((0,), ((0, 0),)),),
+                    (GriddedPerm((0,), ((2, 2),)),),
+                ),
+                assumptions=(
+                    TrackingAssumption((GriddedPerm((0,), ((0, 0),)),)),
+                    TrackingAssumption(
+                        (GriddedPerm((0,), ((0, 0),)), GriddedPerm((0,), ((2, 2),))),
+                    ),
+                ),
+            ),
+        ),
+        FactorStrategy([[(0, 0)], [(1, 1)], [(2, 2)]])(
+            Tiling(
+                obstructions=(
+                    GriddedPerm((0, 1), ((0, 0), (0, 0))),
+                    GriddedPerm((1, 0), ((1, 1), (1, 1))),
+                    GriddedPerm((0, 1), ((2, 2), (2, 2))),
+                    GriddedPerm((1, 0), ((2, 2), (2, 2))),
+                ),
+                requirements=(
+                    (GriddedPerm((0,), ((0, 0),)),),
+                    (GriddedPerm((0,), ((2, 2),)),),
+                ),
+                assumptions=(
+                    TrackingAssumption((GriddedPerm((0,), ((0, 0),)),)),
+                    TrackingAssumption((GriddedPerm((0,), ((2, 2),)),)),
+                ),
+            ),
+        ),
         list(
             SlidingFactory(use_symmetries=True)(
                 Tiling(
@@ -183,6 +250,13 @@ def test_sanity_check_rules(rules_to_check):
         print(rule)
         for n in range(6):
             assert rule.sanity_check(n)
+
+        if isinstance(rule.constructor, (CartesianProduct, DisjointUnion)):
+            for idx in range(len(rule.children)):
+                reversed_rule = rule.to_reverse_rule(idx)
+                print(reversed_rule)
+                for n in range(6):
+                    assert reversed_rule.sanity_check(n)
 
 
 def test_sanity_check_big_row_placement():
