3a78128492399915d397a1deac59bb02072dba04
Émile Nadeau <nadeau.emile@gmail.com>
2021-04-19 15:32:28 +0000

    remove test_temp

--- tests/test_temp.py
+++
@@ -1,7 +0,0 @@
-from comb_spec_searcher.bijection import ParallelSpecFinder
-from permuta.permutils.statistics import PermutationStatistic
-
-
-def test_something_only_on_dev():
-    assert ParallelSpecFinder._INVALID == -1
-    assert PermutationStatistic.get_by_index(2).name == "Major index"
