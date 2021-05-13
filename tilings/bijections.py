ddeb446a3452bc275b91adf04ea638ae3f9a3760
JonSteinn <jonsteinn@gmail.com>
2021-05-13 17:10:44 +0000

    comment bijection.py

--- tilings/bijections.py
+++ tilings/bijections.py
@@ -1,53 +1,57 @@
 from collections import deque
-from itertools import chain
 from typing import Deque, Dict, Optional, Set, Tuple
 
-from comb_spec_searcher.bijection import (
-    EqPathParallelSpecFinder,
-    ParallelSpecFinder,
-    SpecMap,
-)
+from comb_spec_searcher.bijection import EqPathParallelSpecFinder, SpecMap
 from comb_spec_searcher.comb_spec_searcher import CombinatorialSpecificationSearcher
 from comb_spec_searcher.isomorphism import Bijection, Isomorphism
 from comb_spec_searcher.specification import CombinatorialSpecification
 from comb_spec_searcher.specification_extrator import RulePathToAtomExtractor
 from comb_spec_searcher.strategies.rule import AbstractRule, ReverseRule, Rule
-from comb_spec_searcher.strategies.strategy_pack import StrategyPack
 from tilings import GriddedPerm, Tiling
 from tilings.assumptions import TrackingAssumption
 from tilings.strategies import BasicVerificationStrategy
-from tilings.strategies.assumption_insertion import AddAssumptionsStrategy
-from tilings.strategies.fusion import FusionStrategy
 from tilings.strategies.fusion.fusion import FusionRule
-from tilings.strategies.rearrange_assumption import RearrangeAssumptionStrategy
-from tilings.tilescope import TileScope
 
 AssumptionLabels = Dict[int, Set[TrackingAssumption]]
 
 
 class _AssumptionPathTracker:
+    """Tracks assumptions down to atoms with labels."""
+
     def __init__(
         self,
         path1: Deque[Tuple[AbstractRule[Tiling, GriddedPerm], int]],
         path2: Deque[Tuple[AbstractRule[Tiling, GriddedPerm], int]],
     ) -> None:
+        if 0 in (len(path1), len(path2)):
+            raise ValueError("Paths should contain at least one rule.")
+
+        # Number of non-equivalence rules should be the same
         assert sum(1 for r, _ in path1 if not r.is_equivalence()) == sum(
             1 for r, _ in path2 if not r.is_equivalence()
         )
+
         self.nxt_label = 0
         self.path1 = path1
         self.path2 = path2
+
+        # rules and indices set to that of root and first child in each path
         self.r1, self.idx1 = _AssumptionPathTracker._get_next(path1)
         self.r2, self.idx2 = _AssumptionPathTracker._get_next(path2)
+
+        # If root contains any assumptions, they are gathered here
+        # These are a map of labels (ids) to assumptions
         self.assumptions1, self.assumptions2 = self._init_assumptions()
 
     def _init_assumptions(self) -> Tuple[AssumptionLabels, AssumptionLabels]:
         a1: AssumptionLabels = {}
         a2: AssumptionLabels = {}
+        # If any label at root, we store them at the first label
         if self.r1.comb_class.assumptions:
             a1[self.nxt_label] = set(self.r1.comb_class.assumptions)
         if self.r2.comb_class.assumptions:
             a2[self.nxt_label] = set(self.r2.comb_class.assumptions)
+        # Increment next label if root had any assumptions
         self.nxt_label = max(len(a1), len(a2))
         return a1, a2
 
@@ -55,6 +59,7 @@ class _AssumptionPathTracker:
     def _get_next(
         path: Deque[Tuple[AbstractRule, int]]
     ) -> Tuple[Rule[Tiling, GriddedPerm], int]:
+        """Get next rule and the index of its child we traverse through."""
         r, idx = path.popleft()
         assert isinstance(r, Rule)
         assert not path or r.children[idx] == path[0][0].comb_class
@@ -62,9 +67,14 @@ class _AssumptionPathTracker:
 
     @staticmethod
     def _get_par_map(rule: Rule[Tiling, GriddedPerm], idx: int) -> Dict[str, str]:
+        """Given a rule and an index of its child, give the extra parameters map from
+        the parent to that child.
+        """
         if not isinstance(rule, ReverseRule):
             return rule.strategy.extra_parameters(rule.comb_class, rule.children)[idx]
-        assert len(rule.children) == 1  # We currently only have 1-1 rev rules
+        # If the rule is a reverse rule, we assume that it has only one children
+        # as that is the only used reverse rule that have been implemented so far.
+        assert len(rule.children) == 1
         return rule.strategy.extra_parameters(rule.children[0], (rule.comb_class,))[idx]
 
     @staticmethod
@@ -73,12 +83,17 @@ class _AssumptionPathTracker:
         rule: Rule[Tiling, GriddedPerm],
         idx: int,
     ) -> AssumptionLabels:
+        """Recreate the dictionary of labels to assumptions by mapping each assumption
+        to the corresponding assumption in the idx-th child of rule, if there is any.
+        If the label's value becomes empty, we remove it.
+        """
         d: AssumptionLabels = {}
         parmap = _AssumptionPathTracker._get_par_map(rule, idx)
         for i, s in assumptions.items():
             new_set = set()
             for assumption in s:
                 parent_var = rule.comb_class.get_assumption_parameter(assumption)
+                # If parent's assumption is in child
                 if parent_var in parmap:
                     child_var = parmap[parent_var]
                     child_assumption = rule.children[idx].get_assumption(child_var)
@@ -88,6 +103,9 @@ class _AssumptionPathTracker:
         return d
 
     def _step_through_equivalences(self):
+        """
+        Map assumptions though equivalences assuming they won't introduce new ones.
+        """
         while self.r1.is_equivalence():
             self.assumptions1 = _AssumptionPathTracker._forward_all_labels(
                 self.assumptions1, self.r1, self.idx1
@@ -100,6 +118,9 @@ class _AssumptionPathTracker:
             self.r2, self.idx2 = _AssumptionPathTracker._get_next(self.path2)
 
     def _step_forward(self) -> None:
+        """
+        A step forward will perform the map for one non-equivalence rule for each path.
+        """
         self._step_through_equivalences()
         self.assumptions1 = _AssumptionPathTracker._forward_all_labels(
             self.assumptions1, self.r1, self.idx1
@@ -113,6 +134,7 @@ class _AssumptionPathTracker:
         rule: Rule[Tiling, GriddedPerm],
         assumptions: AssumptionLabels,
     ) -> Set[TrackingAssumption]:
+        """The assumption in the current class that don't correspond to any label."""
         return set(
             assumption
             for assumption in rule.comb_class.assumptions
@@ -122,34 +144,41 @@ class _AssumptionPathTracker:
     def _check_for_new_assumptions(self) -> None:
         a1 = _AssumptionPathTracker._find_new_assumptions(self.r1, self.assumptions1)
         a2 = _AssumptionPathTracker._find_new_assumptions(self.r2, self.assumptions2)
+
+        # Add any new atoms to the label tra
         if a1:
             self.assumptions1[self.nxt_label] = a1
         if a2:
             self.assumptions2[self.nxt_label] = a2
+
+        # Increment the label system if new ones
         if a1 or a2:
             self.nxt_label += 1
 
     def assumptions_match_down_to_atom(self) -> bool:
         while True:
+            # Map assumptions to next node
             self._step_forward()
+
+            # If there aren't any more rules, we are done as we should be at an atom now
             if not self.path1 and not self.path2:
                 break
+
+            # Assign next rules
             self.r1, self.idx1 = _AssumptionPathTracker._get_next(self.path1)
             self.r2, self.idx2 = _AssumptionPathTracker._get_next(self.path2)
+
+            # Search for new assumptions that will recieve a new label
             self._check_for_new_assumptions()
 
+        # When at the atoms, the labels should match
         return sorted(self.assumptions1.keys()) == sorted(self.assumptions2.keys())
 
 
 class FusionParallelSpecFinder(
     EqPathParallelSpecFinder[Tiling, GriddedPerm, Tiling, GriddedPerm]
 ):
-    def __init__(
-        self,
-        searcher1: TileScope,
-        searcher2: TileScope,
-    ):
-        super().__init__(searcher1, searcher2)
+    """A specialized parallel specification finder for fusion."""
 
     def _pre_expand(
         self,
@@ -191,6 +220,8 @@ class FusionParallelSpecFinder(
 
 
 class FusionIsomorphism(Isomorphism[Tiling, GriddedPerm, Tiling, GriddedPerm]):
+    """Does additional checks for atoms and fusion constructors."""
+
     def _atom_match(
         self,
         atom1: Tiling,
@@ -208,7 +239,9 @@ class FusionIsomorphism(Isomorphism[Tiling, GriddedPerm, Tiling, GriddedPerm]):
         path1: Deque[Tuple[AbstractRule[Tiling, GriddedPerm], int]] = deque([])
         path2: Deque[Tuple[AbstractRule[Tiling, GriddedPerm], int]] = deque([])
         curr1, curr2 = self.root1, self.root2
+        # Collect the rules and child indices down to our current atom
         for step1, step2 in self._path_tracker:
+            # spec 1
             for idx in step1:
                 rule = self._rules1[curr1]
                 i = tuple(j for j, c in enumerate(rule.children) if not c.is_empty())[
@@ -216,6 +249,7 @@ class FusionIsomorphism(Isomorphism[Tiling, GriddedPerm, Tiling, GriddedPerm]):
                 ]
                 path1.append((rule, i))
                 curr1 = rule.children[i]
+            # spec 2
             for idx in step2:
                 rule = self._rules2[curr2]
                 i = tuple(j for j, c in enumerate(rule.children) if not c.is_empty())[
@@ -238,17 +272,21 @@ class FusionIsomorphism(Isomorphism[Tiling, GriddedPerm, Tiling, GriddedPerm]):
         if not are_eq:
             return False
         assert isinstance(data, list) and 0 < len(data) < 3
+        # If data contains two values, we can't determine the direction of the match
         if (len(data) == 1 and data[0]) or (
             len(data) == 2 and self._fusion_reverse_match(rule1, rule2)
         ):
-            self._index_data[(curr1, curr2)] = True
+            self._index_data[(curr1, curr2)] = True  # Reverse match cached
         return True
 
     def _fusion_reverse_match(self, rule1: FusionRule, rule2: FusionRule) -> bool:
+        # Find fused parameter in parents by going downwards
         parent_param1 = FusionIsomorphism._assumption_bfs_to_parent(rule1, self._rules1)
         parent_param2 = FusionIsomorphism._assumption_bfs_to_parent(rule2, self._rules2)
+        # If we fail to find parent in either, we use normal matching
         if parent_param1 is None or parent_param2 is None:
             return False
+        # Reverse match if there is a mismatch in where the parameters land
         return (
             parent_param1 in rule1.constructor.left_sided_parameters
             and parent_param2 in rule2.constructor.right_sided_parameters
@@ -269,6 +307,7 @@ class FusionIsomorphism(Isomorphism[Tiling, GriddedPerm, Tiling, GriddedPerm]):
         fusion_rule: FusionRule,
         rules: Dict[Tiling, AbstractRule[Tiling, GriddedPerm]],
     ) -> Optional[str]:
+        """A memory enhanced bfs to find parent that tracks parameter."""
         mem: Set[Tiling] = {fusion_rule.children[0]}
         queue: Deque[Tuple[Tiling, str]] = deque(
             [(fusion_rule.children[0], fusion_rule.constructor.fuse_parameter)]
@@ -287,6 +326,8 @@ class FusionIsomorphism(Isomorphism[Tiling, GriddedPerm, Tiling, GriddedPerm]):
 
 
 class FusionBijection(Bijection[Tiling, GriddedPerm, Tiling, GriddedPerm]):
+    """Replaces base Isomorphism with FusionIsomorphism."""
+
     @classmethod
     def construct(
         cls,
@@ -297,34 +338,3 @@ class FusionBijection(Bijection[Tiling, GriddedPerm, Tiling, GriddedPerm]):
         if not iso.are_isomorphic():
             return None
         return cls(spec, other, iso.get_order(), iso.get_order_data())
-
-
-class TilingBijectionFinder:
-    @staticmethod
-    def is_fusion_pack(pack: StrategyPack) -> bool:
-        return any(
-            isinstance(
-                strat,
-                (FusionStrategy, AddAssumptionsStrategy, RearrangeAssumptionStrategy),
-            )
-            for strat in chain(
-                pack.initial_strats, pack.inferral_strats, pack.inferral_strats
-            )
-        )
-
-    @staticmethod
-    def find_bijection_between(
-        searcher1: TileScope, searcher2: TileScope
-    ) -> Optional[Bijection]:
-        if TilingBijectionFinder.is_fusion_pack(
-            searcher1.strategy_pack
-        ) or TilingBijectionFinder.is_fusion_pack(searcher2.strategy_pack):
-            specs = FusionParallelSpecFinder(searcher1, searcher2).find()
-            if specs:
-                return FusionBijection.construct(*specs)
-            return None
-        specs = ParallelSpecFinder[Tiling, GriddedPerm, Tiling, GriddedPerm](
-            searcher1, searcher2
-        ).find()
-        if specs:
-            return Bijection.construct(*specs)
