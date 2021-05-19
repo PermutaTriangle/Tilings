f3c3bd931fd0cbba49d3973dca3b6ef6efbf2a92
Christian Bean <christianbean@ru.is>
2021-05-19 20:14:14 +0000

    a tracked searcher

--- tilings/tilescope.py
+++ tilings/tilescope.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Optional, Tuple, Union
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
 
 import requests
 from logzero import logger
@@ -8,9 +8,14 @@ from comb_spec_searcher import (
     CombinatorialSpecificationSearcher,
 )
 from comb_spec_searcher.rule_db.abstract import RuleDBAbstract
+from comb_spec_searcher.strategies import AbstractStrategy
+from comb_spec_searcher.strategies.rule import AbstractRule
 from comb_spec_searcher.typing import CombinatorialClassType, CSSstrategy
+from comb_spec_searcher.utils import cssmethodtimer
 from permuta import Basis, Perm
 from tilings import GriddedPerm, Tiling
+from tilings.strategies import AddAssumptionFactory, RearrangeAssumptionFactory
+from tilings.strategies.assumption_insertion import AddAssumptionsStrategy
 from tilings.strategy_pack import TileScopePack
 
 __all__ = ("TileScope", "TileScopePack", "LimitedAssumptionTileScope", "GuidedSearcher")
@@ -149,3 +154,81 @@ class GuidedSearcher(TileScope):
         spec = CombinatorialSpecification.from_dict(response.json()["specification"])
         pack = TileScopePack.from_dict(response.json()["pack"]).make_tracked()
         return cls.from_spec(spec, pack)
+
+
+class TrackedSearcher(LimitedAssumptionTileScope):
+    """
+    A TileScope that keeps track of the strategies that apply to Tilings without
+    assumptions and applies these to any tiling with assumptions.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.assumptionless_strategies: Dict[int, List[AbstractStrategy]] = dict()
+        self.expanded = set()
+        super().__init__(*args, **kwargs)
+
+    # @cssmethodtimer("add rule") # called recursively so shouldn't time it.
+    def add_rule(
+        self, start_label: int, end_labels: Tuple[int, ...], rule: AbstractRule
+    ) -> None:
+        if not rule.comb_class.assumptions:
+            if start_label not in self.assumptionless_strategies:
+                self.assumptionless_strategies[start_label] = [
+                    rule.strategy,
+                ]
+            else:
+                self.assumptionless_strategies[start_label].append(rule.strategy)
+        for comb_class, child_label in zip(rule.children, end_labels):
+            if self.symmetries and child_label not in self.symmetry_expanded:
+                self._symmetry_expand(
+                    comb_class, child_label
+                )  # TODO: mark symmetries as empty where appropriate
+            if comb_class.assumptions:
+                if self.expand_sooner(comb_class, child_label):
+                    # print("SKIPPING")
+                    # print(comb_class)
+                    # input()
+                    continue
+                # print("ADDING")
+                # print(comb_class)
+                # input()
+            if rule.workable:
+                # assert child_label >= self.classdb.get_label(
+                #     comb_class.remove_assumptions()
+                # )
+                self.classqueue.add(child_label)
+            if not rule.inferrable:
+                self.classqueue.set_not_inferrable(child_label)
+            if not rule.possibly_empty:
+                self.classdb.set_empty(child_label, empty=False)
+            self.try_verify(comb_class, child_label)
+        if rule.ignore_parent:
+            self.classqueue.set_stop_yielding(start_label)
+        self.ruledb.add(start_label, end_labels, rule)
+
+    def expand_sooner(self, comb_class: Tiling, label: int) -> bool:
+        """
+        Return True if all strategies have been tried, i.e., the underlying tiling
+        has been fully expanded.
+        """
+        if label in self.expanded:
+            return True
+        self.expanded.add(label)
+        underlying_label = self.classdb.get_label(comb_class.remove_assumptions())
+        if underlying_label in self.assumptionless_strategies:
+            for strategy in [
+                AddAssumptionFactory(),
+                RearrangeAssumptionFactory(),
+            ] + self.assumptionless_strategies[underlying_label]:
+                if isinstance(strategy, AddAssumptionsStrategy):
+                    continue
+                for start_label, end_labels, rule in self._expand_class_with_strategy(
+                    comb_class, strategy, label
+                ):
+                    if all(
+                        len(child.assumptions) <= self.max_assumptions
+                        for child in rule.children
+                    ):
+                        self.add_rule(start_label, end_labels, rule)
+            return underlying_label in self.classqueue.ignore
+        return False
