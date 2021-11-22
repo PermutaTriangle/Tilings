b3d7a199a9e5d6ff7ee69cacd62534ac2a8f72ff
Christian Bean <christianbean@ru.is>
2021-11-22 15:49:47 +0000

    adding tests for our successful trees (with xfail for now)

--- tests/test_tilescope.py
+++ tests/test_tilescope.py
@@ -111,6 +111,69 @@ def test_123():
     assert isinstance(spec, CombinatorialSpecification)
 
 
+@pytest.mark.timeout(60)
+def test_123_pp_fusion():
+    searcher = TileScope(
+        (Perm((0, 1, 2)),), TileScopePack.point_placements().make_fusion(tracked=True)
+    )
+    spec = searcher.auto_search()
+    assert isinstance(spec, CombinatorialSpecification)
+    assert [spec.count_objects_of_size(i) for i in range(20)] == [
+        1,
+        1,
+        2,
+        5,
+        14,
+        42,
+        132,
+        429,
+        1430,
+        4862,
+        16796,
+        58786,
+        208012,
+        742900,
+        2674440,
+        9694845,
+        35357670,
+        129644790,
+        477638700,
+        1767263190,
+    ]
+
+
+@pytest.mark.timeout(20)
+def test_123_row_fusion():
+    searcher = TileScope(
+        (Perm((0, 1, 2)),),
+        TileScopePack.row_and_col_placements(row_only=True).make_fusion(tracked=True),
+    )
+    spec = searcher.auto_search()
+    assert isinstance(spec, CombinatorialSpecification)
+    assert [spec.count_objects_of_size(i) for i in range(20)] == [
+        1,
+        1,
+        2,
+        5,
+        14,
+        42,
+        132,
+        429,
+        1430,
+        4862,
+        16796,
+        58786,
+        208012,
+        742900,
+        2674440,
+        9694845,
+        35357670,
+        129644790,
+        477638700,
+        1767263190,
+    ]
+
+
 @pytest.mark.timeout(120)
 def test_123_with_db():
     searcher = TileScope("123", all_the_strategies_verify_database)
@@ -121,13 +184,125 @@ def test_123_with_db():
 @pytest.mark.xfail
 @pytest.mark.timeout(20)
 def test_1342_1423():
-    point_placements_component_fusion = point_placements.make_fusion(
-        component=True, tracked=False
-    )
+    point_placements_component_fusion = point_placements.make_fusion(tracked=True)
     searcher = TileScope("1342_1423", point_placements_component_fusion)
     spec = searcher.auto_search(smallest=True)
-    assert spec.number_of_rules() == 9
-    assert isinstance(spec, CombinatorialSpecification)
+    assert [spec.count_objects_of_size(i) for i in range(12)] == [
+        1,
+        1,
+        2,
+        6,
+        22,
+        90,
+        394,
+        1806,
+        8558,
+        41586,
+        206098,
+        1037718,
+        5293446,
+        27297738,
+        142078746,
+        745387038,
+        3937603038,
+        20927156706,
+        111818026018,
+        600318853926,
+    ]
+
+
+@pytest.mark.xfail
+@pytest.mark.timeout(20)
+def test_3142_2413():
+    point_placements_component_fusion = point_placements.make_fusion(tracked=True)
+    searcher = TileScope("3142_2413", point_placements_component_fusion)
+    spec = searcher.auto_search(smallest=True)
+    assert [spec.count_objects_of_size(i) for i in range(12)] == [
+        1,
+        1,
+        2,
+        6,
+        22,
+        90,
+        394,
+        1806,
+        8558,
+        41586,
+        206098,
+        1037718,
+        5293446,
+        27297738,
+        142078746,
+        745387038,
+        3937603038,
+        20927156706,
+        111818026018,
+        600318853926,
+    ]
+
+
+@pytest.mark.xfail
+@pytest.mark.timeout(20)
+def test_1234():
+    searcher = TileScope(
+        "1234",
+        TileScopePack.row_and_col_placements(row_only=True).make_fusion(tracked=True),
+    )
+    spec = searcher.auto_search()
+    assert [spec.count_objects_of_size(i) for i in range(20)] == [
+        1,
+        1,
+        2,
+        6,
+        23,
+        103,
+        513,
+        2761,
+        15767,
+        94359,
+        586590,
+        3763290,
+        24792705,
+        167078577,
+        1148208090,
+        8026793118,
+        56963722223,
+        409687815151,
+        2981863943718,
+        21937062144834,
+    ]
+
+
+@pytest.mark.xfail
+@pytest.mark.timeout(20)
+def test_1243():
+    searcher = TileScope(
+        "1243",
+        TileScopePack.row_and_col_placements(row_only=True).make_fusion(tracked=True),
+    )
+    spec = searcher.auto_search()
+    assert [spec.count_objects_of_size(i) for i in range(20)] == [
+        1,
+        1,
+        2,
+        6,
+        23,
+        103,
+        513,
+        2761,
+        15767,
+        94359,
+        586590,
+        3763290,
+        24792705,
+        167078577,
+        1148208090,
+        8026793118,
+        56963722223,
+        409687815151,
+        2981863943718,
+        21937062144834,
+    ]
 
 
 @pytest.mark.timeout(60)
@@ -316,9 +491,7 @@ def test_domino():
     )
     tilescope = TileScope(
         domino,
-        TileScopePack.row_and_col_placements().make_fusion(
-            tracked=True, component=True
-        ),
+        TileScopePack.row_and_col_placements(col_only=True).make_fusion(tracked=True),
     )
     spec = tilescope.auto_search()
     assert isinstance(spec, CombinatorialSpecification)
@@ -383,7 +556,8 @@ def forest_expansion():
     ]
 
 
-@pytest.mark.skip(reason="seems to run forever")
+@pytest.mark.xfail
+@pytest.mark.timeout(20)
 def test_guided_searcher():
     tilescope = TileScope(
         "123", TileScopePack.point_placements().make_fusion(tracked=False)
