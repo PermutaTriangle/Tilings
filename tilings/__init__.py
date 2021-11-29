18b272a138d6d477b03f7d20344d631c9e43e248
Christian Bean <christianbean@ru.is>
2021-11-29 15:34:35 +0000

    revert adding rowcolmap, param, preim to init due to circular import

--- tilings/__init__.py
+++ tilings/__init__.py
@@ -1,16 +1,11 @@
 from tilings.assumptions import TrackingAssumption
 from tilings.griddedperm import GriddedPerm
-from tilings.map import RowColMap
-from tilings.parameter_counter import ParameterCounter, PreimageCounter
 from tilings.tiling import Tiling
 
 __version__ = "3.0.0"
 
 __all__ = [
     "GriddedPerm",
-    "ParameterCounter",
-    "PreimageCounter",
-    "RowColMap",
     "Tiling",
     "TrackingAssumption",
 ]
