0c64e3879b6b664614e68452c25fb5c30a5be6eb
Christian Bean <christianbean93@gmail.com>
2023-01-19 13:56:21 +0000

    Sliding bug (#501)
    * add consecutive value condition for sliding
    
    * tidy up

--- tests/strategies/test_sliding.py
+++ tests/strategies/test_sliding.py
@@ -1,5 +1,6 @@
 from tilings import GriddedPerm, Tiling
 from tilings.assumptions import TrackingAssumption
+from tilings.strategies.monotone_sliding import MonotoneSlidingFactory
 from tilings.strategies.sliding import SlidingFactory
 
 tiling = Tiling(
@@ -31,6 +32,70 @@ tiling = Tiling(
     ),
 )
 
+noslidetiling1 = Tiling(
+    obstructions=(
+        GriddedPerm((0, 1), ((0, 0), (0, 0))),
+        GriddedPerm((0, 1, 2), ((1, 0), (1, 0), (1, 0))),
+        GriddedPerm((0, 1, 2, 3), ((2, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 3, 2), ((2, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 2, 3, 4), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 4, 3), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 3, 2, 4), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 3, 4, 2), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 4, 2, 3), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 4, 3, 2), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 2, 3, 4, 1), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 2, 4, 3, 1), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (1, 0), (1, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (1, 0), (2, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (1, 0), (3, 0))),
+        GriddedPerm((0, 1, 2), ((1, 0), (1, 0), (2, 0))),
+        GriddedPerm((0, 1, 2), ((1, 0), (1, 0), (3, 0))),
+        GriddedPerm((0, 2, 1), ((0, 0), (1, 0), (3, 0))),
+        GriddedPerm((0, 2, 1), ((1, 0), (1, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((0, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 2, 3), ((0, 0), (2, 0), (2, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((0, 0), (2, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((1, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 2, 3), ((1, 0), (2, 0), (2, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((1, 0), (2, 0), (3, 0), (3, 0))),
+    ),
+    requirements=(),
+    assumptions=[TrackingAssumption((GriddedPerm((0,), ((0, 0),)),))],
+)
+
+noslidetiling2 = Tiling(
+    obstructions=(
+        GriddedPerm((0, 1), ((1, 0), (1, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (0, 0), (0, 0))),
+        GriddedPerm((0, 1, 2, 3), ((2, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 3, 2), ((2, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 2, 3, 4), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 4, 3), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 3, 2, 4), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 3, 4, 2), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 4, 2, 3), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 4, 3, 2), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 2, 3, 4, 1), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 2, 4, 3, 1), ((3, 0), (3, 0), (3, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (0, 0), (1, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (0, 0), (2, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (0, 0), (3, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (1, 0), (2, 0))),
+        GriddedPerm((0, 1, 2), ((0, 0), (1, 0), (3, 0))),
+        GriddedPerm((0, 2, 1), ((0, 0), (0, 0), (3, 0))),
+        GriddedPerm((0, 2, 1), ((0, 0), (1, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((0, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 2, 3), ((0, 0), (2, 0), (2, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((0, 0), (2, 0), (3, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((1, 0), (2, 0), (2, 0), (2, 0))),
+        GriddedPerm((0, 1, 2, 3), ((1, 0), (2, 0), (2, 0), (3, 0))),
+        GriddedPerm((0, 1, 2, 3), ((1, 0), (2, 0), (3, 0), (3, 0))),
+    ),
+    requirements=(),
+    assumptions=[TrackingAssumption((GriddedPerm((0,), ((1, 0),)),))],
+)
+
 
 def sanity_checker(rules):
     found_some = False
@@ -46,3 +111,10 @@ def test_sliding_factory():
     assert sanity_checker(SlidingFactory(True)(tiling.reverse()))
     assert sanity_checker(SlidingFactory(True)(tiling.rotate90()))
     assert sanity_checker(SlidingFactory(True)(tiling.rotate90().reverse()))
+
+
+def test_monotone_sliding_factory():
+    assert list(MonotoneSlidingFactory()(noslidetiling1)) == []
+    assert list(MonotoneSlidingFactory()(noslidetiling2)) == []
+    for strategy in MonotoneSlidingFactory()(tiling):
+        assert sanity_checker([strategy(tiling)])
